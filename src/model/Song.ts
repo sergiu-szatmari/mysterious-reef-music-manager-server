bc41373109383010caebaf97a5a19d5c3df455e4
Sergiu <szatmari.sergiu@gmail.com>
2020-08-06 13:54:21 +0300

    fixes @ mongodb validators

--- src/model/Song.ts
+++ src/model/Song.ts
@@ -1,30 +1,32 @@
 import mongoose, { Schema } from 'mongoose';
 import config from 'config';
 
-import { MongoDBValidator } from '../middleware';
-
 export const SongCollectionName: string = `${config.get('General.dbConfig.collectionPrefix')}songs`;
 export const SongSchema: Schema = new Schema({
     name: {
         type: String,
         required: [true, 'Song\'s name is required'],
-        // validate: {
-        //     validator: MongoDBValidator.validateString,
-        //     message: props => `${props.value} is invalid`
-        // }
     },
-    duration: { type: Number, required: [true, 'Song\'s duration is required'] },
+    duration: {
+        type: Number,
+        required: [true, 'Song\'s duration is required']
+    },
     genre: [{
         type: String,
         required: [true, 'Song\'s genre is required'],
-        // validate: {
-        //     validator: MongoDBValidator.validateString,
-        //     message: (props: any) => `${props.value} is invalid`
-        // }
     }],
-    bpm: { type: Number, required: [true, 'Song\'s bpm is required'] },
-    artistID: { type: Schema.Types.ObjectId, required: [true, 'Song\'s artistID is required'] },
-    dateAdded: { type: Date, default: Date.now }
+    bpm: {
+        type: Number,
+        required: [true, 'Song\'s bpm is required']
+    },
+    artistID: {
+        type: Schema.Types.ObjectId,
+        required: [true, 'Song\'s artistID is required']
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 export const Song = mongoose.model('Song', SongSchema, SongCollectionName);
